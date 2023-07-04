5fcc5acf65136642631fbd6c5447f7f8c3d497b3
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2023-07-04 20:07:22 +0200

    Test de compornar elemento solicitado correctamente hecho

--- PJPF/src/test/java/ufv/dis/final2023/example/PJPF/ControllerTest.java
+++ PJPF/src/test/java/ufv/dis/final2023/example/PJPF/ControllerTest.java
@@ -1,5 +1,7 @@
 package ufv.dis.final2023.example.PJPF;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class ControllerTest {
@@ -7,7 +9,19 @@ public class ControllerTest {
     @org.junit.Test
     public void mandarElemento()
     {
+        Controller cont = new Controller();
+        DataHanding data = new DataHanding();
         Params param = new Params("people",4);
-        
+        ArrayList<String> films= new ArrayList<String>();
+        films.add("https://swapi.dev/api/films/1/");
+        films.add("https://swapi.dev/api/films/2/");
+        films.add("https://swapi.dev/api/films/3/");
+        films.add("hhttps://swapi.dev/api/films/6/");
+
+        People people = new People("Darth Vader","136" ,"male",films);
+
+        cont.mandarElemento(param);
+
+
     }
 }
