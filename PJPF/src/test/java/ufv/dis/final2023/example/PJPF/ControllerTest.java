9f8678327ca26b1dbf30ac4049d91945195a5a13
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2023-07-04 19:58:34 +0200

    Creada clases para test

---
+++ PJPF/src/test/java/ufv/dis/final2023/example/PJPF/ControllerTest.java
@@ -0,0 +1,13 @@
+package ufv.dis.final2023.example.PJPF;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+    @org.junit.Test
+    public void mandarElemento()
+    {
+        Params param = new Params("people",4);
+        
+    }
+}
