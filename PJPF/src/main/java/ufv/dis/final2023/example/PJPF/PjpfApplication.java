4d5e598ae3c1899be51cefb088153c5f1bac93f1
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2023-07-04 19:17:57 +0200

    Creada plantilla para el examen

---
+++ PJPF/src/main/java/ufv/dis/final2023/example/PJPF/PjpfApplication.java
@@ -0,0 +1,13 @@
+package ufv.dis.final2023.example.PJPF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjpfApplication.class, args);
+	}
+
+}
---
+++ PJPF/src/test/java/ufv/dis/final2023/example/PJPF/PjpfApplicationTests.java
@@ -0,0 +1,13 @@
+package ufv.dis.final2023.example.PJPF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjpfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
